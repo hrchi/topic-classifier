854a68766a9a038fa8ab28c1d237c5d9fc1c7aee
Hamidreza <hchinae@gmail.com>
2025-05-18 15:39:36 +0000

    Update from Colab added the clone, and stup of url for push using token

--- model/dnn.py
+++ model/dnn.py
@@ -1,1 +1,19 @@
+import torch.nn as nn
 
+class DNNClassifier(nn.Module):
+    def __init__(self, vocab_size, embedding_dim, hidden_dim, num_classes, dropout=0.3):
+        super().__init__()
+        self.embedding = nn.Embedding(vocab_size, embedding_dim)
+        self.fc1 = nn.Linear(embedding_dim, hidden_dim)
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(dropout)
+        self.fc2 = nn.Linear(hidden_dim, num_classes)
+
+    def forward(self, x):
+        embedded = self.embedding(x)
+        pooled = embedded.mean(dim=1)
+        x = self.fc1(pooled)
+        x = self.relu(x)
+        x = self.dropout(x)
+        x = self.fc2(x)
+        return x
